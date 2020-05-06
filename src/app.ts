d9f592d7e2a4d7d299a722722e74d8bdc0ac576e
sandeeppal6 <knowledge.base743@gmail.com>
2020-05-06 10:34:20 +0530

    setup mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 import * as express from "express";
 import { Application, Request, Response, NextFunction } from "express";
 
-const PORT = process.env.PORT || 5000;
 const app: Application = express();
 
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+
+mongoose.connect(
+  `${DB_URL}${DATABASE_NAME}`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) console.error(err);
+    console.log("connected to mongodb");
+  }
+);
 
 app.use(cors());
 
@@ -13,5 +27,5 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(PORT, () => {
-  console.log("server running......");
+  console.log(`server running on port ${PORT}`);
 });
